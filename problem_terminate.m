508484834042b0c5656ccf01fdee1c657641a843
ibrahim <ibrahimyildirimfr@gmail.com>
2024-12-15 18:49:45 +0300

    source code

---
+++ problem_terminate.m
@@ -0,0 +1,19 @@
+function [ecosize, n, maxFE, lb, ub] = problem_terminate()
+    % Parameter settings:
+    
+    % ecosystem (population) size
+    ecosize = 50;
+    % problem dimension
+    n = 30;
+    % number of function evaluations
+    maxFE = 100 * n;
+    % problem lower band 
+    lowerBand = -100;
+    lb = ones(1, n) * lowerBand;
+    % problem upper band
+    upperBand = 100;
+    ub = ones(1, n) * upperBand;
+end
+
+
+
